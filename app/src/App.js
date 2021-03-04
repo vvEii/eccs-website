34ac7bed2f9a2dfcec9d7ea56a044f42d7ad8efc
vvEii <coopershang125@gmail.com>
2021-03-03 18:11:19 -0800

    Add home and about routes

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,9 @@
 //import logo from './logo.svg';
+import { Route, Switch } from 'react-router';
 import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
@@ -21,15 +25,11 @@ function App() {
       {/* <div className="bg-no-repeat bg-center h-5/6" style={{background: `url('https://images.squarespace-cdn.com/content/5d375649e2ffc000016e0aec/1566216421263-WABITDNT9SVT641I149X/20140302_Trade%2B151_0503.jpg?format=2500w&content-type=image%2Fjpeg')` }}>
       asd
       </div> */}
-      <div
-        className='bg-no-repeat bg-center w-screen h-screen py-8'
-        style={{
-          backgroundImage:
-            'url(https://images.squarespace-cdn.com/content/5d375649e2ffc000016e0aec/1566216421263-WABITDNT9SVT641I149X/20140302_Trade%2B151_0503.jpg?format=2500w&content-type=image%2Fjpeg)',
-        }}
-      ></div>
-
-      <h1 className='text-lg font-normal'>AAAAGG</h1>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+      </Switch>
     </div>
   );
 }
---
+++ app/src/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <div className='bg-yellow-300 text-5xl'>About</div>
+    </div>
+  );
+};
+
+export default About;
