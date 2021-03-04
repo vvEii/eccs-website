1af302fd6cd9acbc1a85e0ba5ca3203db52b1006
vvEii <coopershang125@gmail.com>
2021-03-03 18:00:07 -0800

    Enable ESLint and Prettier

--- app/src/App.js
+++ app/src/App.js
@@ -2,9 +2,8 @@
 import './App.css';
 
 function App() {
-
   return (
-    <div className="App">
+    <div className='App'>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -22,9 +21,14 @@ function App() {
       {/* <div className="bg-no-repeat bg-center h-5/6" style={{background: `url('https://images.squarespace-cdn.com/content/5d375649e2ffc000016e0aec/1566216421263-WABITDNT9SVT641I149X/20140302_Trade%2B151_0503.jpg?format=2500w&content-type=image%2Fjpeg')` }}>
       asd
       </div> */}
-      <div className="bg-no-repeat bg-center w-screen h-screen py-8" style={{ backgroundImage: "url(https://images.squarespace-cdn.com/content/5d375649e2ffc000016e0aec/1566216421263-WABITDNT9SVT641I149X/20140302_Trade%2B151_0503.jpg?format=2500w&content-type=image%2Fjpeg)"}} >
-      </div>
-     
+      <div
+        className='bg-no-repeat bg-center w-screen h-screen py-8'
+        style={{
+          backgroundImage:
+            'url(https://images.squarespace-cdn.com/content/5d375649e2ffc000016e0aec/1566216421263-WABITDNT9SVT641I149X/20140302_Trade%2B151_0503.jpg?format=2500w&content-type=image%2Fjpeg)',
+        }}
+      ></div>
+
       <h1 className='text-lg font-normal'>AAAAGG</h1>
     </div>
   );
