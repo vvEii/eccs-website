019a010b30a3a4b6eb1dd04e3e1d3542f49df19c
vvEii <coopershang125@gmail.com>
2021-03-11 15:40:22 -0800

    Add banner to about page

--- app/src/pages/About.js
+++ app/src/pages/About.js
@@ -4,9 +4,14 @@ const About = () => {
   return (
     <>
       <div
-        className='bg-no-repeat bg-center bg-cover bg-cover w-screen relative py-72'
+        className='bg-no-repeat bg-center bg-cover bg-cover w-screen relative py-40'
         style={{ backgroundImage: 'url(images/bg-about.jpg)' }}
-      ></div>
+      >
+        <p className='text-left font-bold text-6xl text-white pl-24 w-3/4 leading-snug'>
+          Since 1950, we have participated in nearly every major public lands
+          victory.
+        </p>
+      </div>
 
       <span className='text-5xl'>content</span>
     </>
--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -11,9 +11,9 @@ const Home = () => {
         }}
       >
         <div className='flex flex-col absolute bottom-40'>
-          <span className='text-5xl text-white font-bold pl-24 w-3/5 mb-10 text-left leading-normal'>
+          <p className='text-5xl text-white font-bold pl-24 w-3/5 mb-10 text-left leading-normal'>
             Help our communities with love and passion
-          </span>
+          </p>
           <Link
             to='/about'
             className='bg-white text-black px-6 py-4 font-black w-40 ml-24 hover:bg-gray-100'
@@ -23,15 +23,15 @@ const Home = () => {
         </div>
       </div>
       <div className='flex justify-between items-center w-screen bg-test px-24 py-12'>
-        <span className='font-bold text-black text-4xl text-left leading-normal pr-12'>
+        <p className='font-bold text-black text-4xl text-left leading-normal pr-12'>
           Engaged Communities Canada Society Engaged Communities Canada Society
           Engaged Communities Canada Society
-        </span>
-        <span className='text-black text-xl text-left leading-loose pl-12'>
+        </p>
+        <p className='text-black text-xl text-left leading-loose pl-12'>
           Our mission is to create international consensus around the climate
           emergency, as well a shared plan for saving the planet’s most
           exceptional wild places.
-        </span>
+        </p>
       </div>
     </>
   );
