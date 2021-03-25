8f456239de0bfaed1675226e16d69970baa9a62c
vvEii <coopershang125@gmail.com>
2021-03-25 11:40:22 -0700

    Make program title responsive

--- app/src/pages/ProgramDetails.js
+++ app/src/pages/ProgramDetails.js
@@ -4,7 +4,9 @@ const ProgramDetails = (props) => {
   const program = props.location.programProps;
   return (
     <div className='flex flex-col justify-items-center items-start mx-24 my-48'>
-      <p className='text-5xl font-medium mb-12'>{program.title}</p>
+      <p className='text-4xl font-normal md:text-5xl md:font-medium mb-12'>
+        {program.title}
+      </p>
       <p className='text-base text-black block whitespace-pre-wrap text-left font-light'>
         {program.content}
       </p>
