a29c454dfc197d4313136cc651ffc566ee8fbacf
vvEii <coopershang125@gmail.com>
2021-03-25 11:13:26 -0700

    Update program data

--- app/src/data/programs.js
+++ app/src/data/programs.js
@@ -1,6 +1,6 @@
 const PROGRAMS = [
   {
-    date: '01/03/2021',
+    id: 1,
     title: 'Overdose Prevention',
     abstract: 'Program of overdose prevention',
     img: 'images/program-img-1.jpg',
@@ -9,23 +9,32 @@ const PROGRAMS = [
     Be clear, be confident and don’t overthink it. The beauty of your story is that it’s going to continue to evolve and your site can evolve with it. Your goal should be to make it feel right for right now. Later will take care of itself. It always does.`,
   },
   {
-    date: '02/03/2021',
+    id: 2,
     title: 'Food Security',
     abstract: 'Program of food security',
     img: 'images/program-img-2.jpg',
+    content: `It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference. \n
+    Don’t worry about sounding professional. Sound like you. There are over 1.5 billion websites out there, but your story is what’s going to separate this one from the rest. If you read the words back and don’t hear your own voice in your head, that’s a good sign you still have more work to do.\n
+    Be clear, be confident and don’t overthink it. The beauty of your story is that it’s going to continue to evolve and your site can evolve with it. Your goal should be to make it feel right for right now. Later will take care of itself. It always does.`,
   },
   {
-    date: '03/03/2021',
+    id: 3,
     title: 'Hygiene Program',
     abstract: `Stylists provide free cuts to Surrey's most vulnerable`,
     img: 'images/program-img-3.jpg',
+    content: `It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference. \n
+    Don’t worry about sounding professional. Sound like you. There are over 1.5 billion websites out there, but your story is what’s going to separate this one from the rest. If you read the words back and don’t hear your own voice in your head, that’s a good sign you still have more work to do.\n
+    Be clear, be confident and don’t overthink it. The beauty of your story is that it’s going to continue to evolve and your site can evolve with it. Your goal should be to make it feel right for right now. Later will take care of itself. It always does.`,
   },
   {
-    date: '04/03/2021',
+    id: 4,
     title: 'EC Sports Academy',
     abstract:
       'Opus Prep has been bringing youth-focused programs to communities',
     img: 'images/program-img-4.jpg',
+    content: `It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference. \n
+    Don’t worry about sounding professional. Sound like you. There are over 1.5 billion websites out there, but your story is what’s going to separate this one from the rest. If you read the words back and don’t hear your own voice in your head, that’s a good sign you still have more work to do.\n
+    Be clear, be confident and don’t overthink it. The beauty of your story is that it’s going to continue to evolve and your site can evolve with it. Your goal should be to make it feel right for right now. Later will take care of itself. It always does.`,
   },
 ];
 
