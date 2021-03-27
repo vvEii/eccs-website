5fd26f8f466704ed84f379b81370859c43dbcb10
vvEii <coopershang125@gmail.com>
2021-03-26 22:50:16 -0700

    Add information empty check

--- app/src/components/Form.js
+++ app/src/components/Form.js
@@ -6,9 +6,23 @@ const Form = () => {
   const [lastname, setLastname] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [nameAlert, setNameAlert] = useState(false);
+  const [emailAlert, setEmailAlert] = useState(false);
+  const [messageAlert, setMessageAlert] = useState('');
 
   const handleSubmit = () => {
-    console.log(firstname + lastname + email + message);
+    if (!firstname || !lastname) {
+      setNameAlert(true);
+      return;
+    }
+    if (!email) {
+      setEmailAlert(true);
+      return;
+    }
+    if (!message) {
+      setMessageAlert(true);
+      return;
+    }
     const params = {
       firstname,
       lastname,
@@ -27,7 +41,17 @@ const Form = () => {
   };
   return (
     <form className='grid w-full' onSubmit={(e) => e.preventDefault()}>
-      <p className='text-base text-black font-light text-left'>Name *</p>
+      <p className='text-base text-black font-light text-left inline'>
+        Name *
+        {nameAlert ? (
+          <span className='text-base text-red-400 ml-4'>
+            Please fill both First Name and Last Name.
+          </span>
+        ) : (
+          <></>
+        )}
+      </p>
+
       <div className='grid grid-cols-2 gap-4 '>
         <div className='text-left'>
           <input
@@ -49,7 +73,14 @@ const Form = () => {
         </div>
       </div>
       <div className='text-left mt-14'>
-        <label className='text-base text-black font-light'>Email *</label>
+        <label className='text-base text-black font-light'>Email *</label>{' '}
+        {emailAlert ? (
+          <span className='text-base font-light text-red-400 ml-4'>
+            Please fill your email address.
+          </span>
+        ) : (
+          <></>
+        )}
         <input
           type='text'
           className='pl-2 text-base text-black  bg-gray-100 block w-full h-full border border-gray-400 focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-4'
@@ -59,6 +90,13 @@ const Form = () => {
       </div>
       <div className='text-left mt-14'>
         <label className='text-base text-black font-light'>Message *</label>
+        {messageAlert ? (
+          <span className='text-base font-light text-red-400 ml-4'>
+            Message can't be empty.
+          </span>
+        ) : (
+          <></>
+        )}
         <textarea
           type='text'
           className='pl-2 pt-2 text-base text-black resize-y bg-gray-100 block w-full h-full border border-gray-400 focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-4'
