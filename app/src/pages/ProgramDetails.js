f9def8aab1f5a1301cb4f180d665cc06df998d72
vvEii <coopershang125@gmail.com>
2021-03-28 00:02:18 -0700

    Make nav tab in program details page working

--- app/src/pages/ProgramDetails.js
+++ app/src/pages/ProgramDetails.js
@@ -2,26 +2,56 @@ import React from 'react';
 import ForwardTab from '../components/ForwardTab';
 import BackTab from '../components/BackTab';
 import PROGRAMS from '../data/programs';
+import { Link } from 'react-router-dom';
 
 const ProgramDetails = (props) => {
   const program = props.location.programProps;
   const currentProgramIndex = program.index;
 
   return (
-    <div className='flex flex-col justify-items-center items-start mx-24 my-48'>
+    <div className='flex flex-col justify-items-center items-start mx-24 mt-48 mb-12'>
       <p className='text-4xl font-normal md:text-5xl md:font-medium mb-12'>
         {program.title}
       </p>
       <p className='text-base text-black block whitespace-pre-wrap text-left font-light'>
         {program.content}
       </p>
-      {currentProgramIndex !== PROGRAMS.length - 1 && (
-        <ForwardTab title={PROGRAMS[currentProgramIndex + 1].title} />
-      )}
-
-      {currentProgramIndex && (
-        <BackTab title={PROGRAMS[currentProgramIndex - 1].title} />
-      )}
+      <div className='flex justify-between w-full mt-20'>
+        {currentProgramIndex ? (
+          <Link
+            to={{
+              pathname: `/program-details/${
+                PROGRAMS[currentProgramIndex - 1].id
+              }`,
+              programProps: {
+                content: PROGRAMS[currentProgramIndex - 1].content,
+                title: PROGRAMS[currentProgramIndex - 1].title,
+                index: currentProgramIndex - 1,
+              },
+            }}
+          >
+            <BackTab title={PROGRAMS[currentProgramIndex - 1].title} />
+          </Link>
+        ) : (
+          <div></div>
+        )}
+        {currentProgramIndex !== PROGRAMS.length - 1 && (
+          <Link
+            to={{
+              pathname: `/program-details/${
+                PROGRAMS[currentProgramIndex + 1].id
+              }`,
+              programProps: {
+                content: PROGRAMS[currentProgramIndex + 1].content,
+                title: PROGRAMS[currentProgramIndex + 1].title,
+                index: currentProgramIndex + 1,
+              },
+            }}
+          >
+            <ForwardTab title={PROGRAMS[currentProgramIndex + 1].title} />
+          </Link>
+        )}
+      </div>
     </div>
   );
 };
