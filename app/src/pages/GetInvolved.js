858800f615a510d2e4bbf9e3ed89976b332544d4
vvEii <coopershang125@gmail.com>
2021-03-29 17:43:49 -0700

    Style the bg-image

--- app/src/pages/GetInvolved.js
+++ app/src/pages/GetInvolved.js
@@ -2,21 +2,19 @@ import React from 'react';
 
 const GetInvolved = () => {
   return (
-    <div>
-      <div className='overflow-hidden max-h-screen'>
-        <img
-          src='/images/bg-landing-page.jpg'
-          alt=''
-          className='object-cover relative min-h-screen'
-        />
-        <div
-          className='absolute inset-0 opacity-50'
-          style={{ backgroundColor: '#2a2829' }}
-        ></div>
-      </div>
+    <div className='h-screen'>
+      <img
+        src='/images/bg-landing-page.jpg'
+        alt=''
+        className='object-cover h-screen w-screen'
+      />
+      <div
+        className='absolute inset-0 opacity-50'
+        style={{ backgroundColor: '#2a2829' }}
+      ></div>
 
-      <div className='flex items-center z-10 absolute inset-0 text-white mx-24 text-left '>
-        <div className='flex-1'>
+      <div className='absolute inset-0 grid grid-cols-1 items-center text-white mx-24 text-left md:grid-cols-3 md:gap-10'>
+        <div className=''>
           <p className='text-4xl font-medium py-10'>Donate</p>
           <p className='font-extralight text-lg'>
             It all begins with an idea. Maybe you want to launch a business.
@@ -24,12 +22,23 @@ const GetInvolved = () => {
             have a creative project to share with the world. Whatever it is, the
             way you tell your story online can make all the difference.
           </p>
-          <button className='bg-white text-sm text-black px-7 py-4 my-10 font-medium hover:bg-gray-200 transition ease-in-out duration-700'>
+          <button className='bg-white text-sm text-black px-7 py-4 my-10 font-medium hover:bg-gray-200 transition ease-in-out duration-700 focus:outline-none'>
             Donate
           </button>
         </div>
-        <div className='flex-1'></div>
-        <div className='flex-1'>
+        <div className=''>
+          <p className='text-4xl font-medium py-10'>Parternership</p>
+          <p className='font-extralight text-lg'>
+            It all begins with an idea. Maybe you want to launch a business.
+            Maybe you want to turn a hobby into something more. Or maybe you
+            have a creative project to share with the world. Whatever it is, the
+            way you tell your story online can make all the difference.
+          </p>
+          <button className='bg-white text-sm text-black px-7 py-4 my-10 font-medium hover:bg-gray-200 transition ease-in-out duration-700 focus:outline-none'>
+            Parternership
+          </button>
+        </div>
+        <div className=''>
           <p className='text-4xl font-medium py-10'>
             Volunteer & Employment Opportunities
           </p>
@@ -39,7 +48,7 @@ const GetInvolved = () => {
             have a creative project to share with the world. Whatever it is, the
             way you tell your story online can make all the difference.
           </p>
-          <button className='bg-white text-sm text-black px-7 py-4 my-10 font-medium hover:bg-gray-200 transition ease-in-out duration-700'>
+          <button className='bg-white text-sm text-black px-7 py-4 my-10 font-medium hover:bg-gray-200 transition ease-in-out duration-700 focus:outline-none'>
             Volunteer
           </button>
         </div>
