c1a990bbfd9a0850b3cbd0a4376eafe576ef8401
vvEii <coopershang125@gmail.com>
2021-04-05 15:04:03 -0700

    Add style to PostCard

--- app/src/components/PostCard.js
+++ app/src/components/PostCard.js
@@ -3,24 +3,16 @@ import { Link } from 'react-router-dom';
 
 const PostCard = (props) => {
   return (
-    <div className='flex space-x-10 space-y-10 w-3/5'>
-      {/* <Link
-        to={{
-          pathname: `/program-details/${props.id}`,
-          programProps: {
-            content: props.content,
-            title: props.title,
-            index: props.index,
-          },
-        }}
-      >
-        <img src={props.image} alt='' className='object-cover w-2/6' />
-      </Link> */}
-      <img src={props.image} alt='' className='object-cover w-2/6' />
+    <div className='flex flex-col space-y-10 w-3/5 md:flex-row md:space-x-10 md:items-end'>
+      <img
+        src={props.image}
+        alt=''
+        className='object-cover md:w-4/5 max-h-80 '
+      />
       <div className='flex flex-col items-start space-y-10 justify-end'>
         <p className='text-lg'>{props.date}</p>
         <Link>
-          <p className='text-3xl font-medium'>{props.title}</p>
+          <p className='text-3xl font-medium text-left'>{props.title}</p>
         </Link>
 
         <p className='text-lg font-normal text-left'>{props.abstract}</p>
