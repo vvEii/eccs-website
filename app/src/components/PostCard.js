b09592b75dff47d334e19cfbb64f1fdef130ae1d
vvEii <coopershang125@gmail.com>
2021-04-05 11:51:45 -0700

    Update blogs date

--- app/src/components/PostCard.js
+++ app/src/components/PostCard.js
@@ -6,7 +6,7 @@ const PostCard = (props) => {
       <img src={props.image} alt='' className='object-cover w-2/6' />
       <div className='flex flex-col items-start space-y-10'>
         <p className='text-lg'>{props.date}</p>
-        <p className='text-xl font-medium'>{props.title}</p>
+        <p className='text-3xl font-medium'>{props.title}</p>
         <p className='text-lg font-normal text-left'>{props.abstract}</p>
       </div>
     </div>
