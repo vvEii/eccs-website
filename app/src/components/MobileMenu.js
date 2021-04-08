a2aa7ebaf4ced8ea979a943ea6d25083c074dd75
vvEii <coopershang125@gmail.com>
2021-04-07 23:42:35 -0700

    Add cancel icon svg

--- app/src/components/MobileMenu.js
+++ app/src/components/MobileMenu.js
@@ -8,7 +8,20 @@ const MobileMenu = (props) => {
         <Link to='/' className='text-2xl font-normal w-1/4 text-left'>
           ECCS
         </Link>
-        <div onClick={props.toggle}>close</div>
+        <div onClick={props.toggle}>
+          <svg
+            className='w-8 h-8'
+            fill='currentColor'
+            viewBox='0 0 20 20'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              fillRule='evenodd'
+              d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z'
+              clipRule='evenodd'
+            />
+          </svg>
+        </div>
       </div>
       <div className='flex flex-col space-y-5'>
         <Link className='font-light text-3xl' to='/blog'>
