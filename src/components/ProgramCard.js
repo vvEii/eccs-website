c98dbfea43f0e877980ae3f53a61d511bda7d529
vvEii <coopershang125@gmail.com>
2021-04-15 15:49:29 -0700

    Optimize mobile style

--- src/components/ProgramCard.js
+++ src/components/ProgramCard.js
@@ -4,7 +4,11 @@ import { Link } from 'react-router-dom';
 const ProgramCard = (props) => {
   return (
     <div className='flex flex-col justify-center items-center col-span-1 row-span-1'>
-      <img src={props.img} alt='Program' className='object-cover h-full' />
+      <img
+        src={props.img}
+        alt='Program'
+        className='object-cover h-48 md:h-full'
+      />
       <div className='text-black text-3xl font-normal pt-8'>{props.title}</div>
       <div className='text-black text-sm pt-8 font-light md:font-normal'>
         {props.abstract}
